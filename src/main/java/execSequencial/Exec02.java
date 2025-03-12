53335375e6a2f596243ba30171d132bb44b1f9b0
Vitor Gabriel <vitorgabriel42@hotmail.com>
2025-03-12 14:32:48 -0300

    Exercise 02 of basic module

--- src/main/java/execSequencial/Exec02.java
+++ src/main/java/execSequencial/Exec02.java
@@ -1,4 +1,24 @@
 package execSequencial;
 
+import java.util.Locale;
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Exec02 {
+    /*
+    Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com
+    quatro casas decimais conforme exemplos.
+    Fórmula da área: area = π . raio2
+    Considere o valor de π = 3.14159
+    */
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner sc = new Scanner(System.in);
+        double pi = 3.14159;
+        double area, raio;
+        System.out.println("Informe o valor do Raio do círculo: ");
+        raio = sc.nextDouble();
+        area = pi * Math.pow(raio, 2);
+        System.out.printf("A=%.4f", area);
+    }
 }
